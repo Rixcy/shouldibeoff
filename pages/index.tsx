fd50dd2abf3cdf0cfd867a2594f62ca77213d362
Rick Booth <admin@rickbooth.me>
2022-04-15 12:48:08 +0100

    feat: add custom favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,24 @@ const Home: NextPage = () => {
           name="description"
           content="Ever wondered if you should be off work today? Is it Good Friday, is it Christmas? This simple site will tell you."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>
